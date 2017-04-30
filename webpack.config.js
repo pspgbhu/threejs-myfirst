fa8d297f70718bca1952b5421a6cda50ba5c5ec8
hucong <hucong@jd.com>
2017-04-30 14:53:09 +0800

    init

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,21 @@
 var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
-    app: 'src/index.js',
+    app: './src/index.js',
   },
-  
+
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name]bundle.js',
+    filename: '[name].bundle.js',
   },
+
+  // plugins: [
+  //   new HtmlWebpackPlugin({
+  //     filename: 'index.html',
+  //     template: 'index.html',
+  //     inject: true,
+  //   }),
+  // ],
 };
